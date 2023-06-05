b65a44bec5d86ff3429de9011adbfd121e2261ab
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2023-06-05 10:48:09 -0300

    Update Servidor_Pratica.js

--- Servico_E_Banco/Servidor_Pratica.js
+++ Servico_E_Banco/Servidor_Pratica.js
@@ -3,10 +3,10 @@ var uv = express();
 var produto = require("./controllers/produtoControllers");
 
 uv.listen(8081,function(){
-    console.log("Servidor em Processamento...")
+    console.log("Servidor em Processamento...");
 })
 uv.get("/", function(req,res){
-    res.send("Seja bem vindo(a) ao meu site!!!")
+    res.send("Seja bem vindo(a) ao meu site!!!");
 })
 
 //
