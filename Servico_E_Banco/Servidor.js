7d64afe06f341921be4831ad4f3e0fd292c3755f
Silas Vasconcelos <75582929+s-v7@users.noreply.github.com>
2023-06-05 10:49:02 -0300

    Update Servidor.js
    Update

--- Servico_E_Banco/Servidor.js
+++ Servico_E_Banco/Servidor.js
@@ -1,37 +1,37 @@
-var express = require("express")
-var svc = express()
+var express = require("express");
+var svc = express();
 
 svc.listen(8081, function(){
-    console.log("Servidor Executando...")
+    console.log("Servidor Executando...");
 })
 //Rota_01
 svc.get("/somar", function(req,res){
-    res.sendFile( __dirname + "/somarElem.html")
+    res.sendFile( __dirname + "/somarElem.html");
 })
 //Rota_02 => /multiplicar/:mul1/mul2
 svc.get("/multiplicacao/:mul1/:mul2", function(req,res){
-    res.send("A multiplicação é "+(req.params.mul1 * req.params.mul2))
+    res.send("A multiplicação é "+(req.params.mul1 * req.params.mul2));
 })
 //Rota raiz que direcinar para outras páginas !!!
 svc.get("/", function(req,res){
-    res.sendFile( __dirname + "/rotas.html")
+    res.sendFile( __dirname + "/rotas.html");
 })
 //outras Páginas
 svc.get("/funcao_k", function(req,res){
-    res.sendFile( __dirname + "/funcao_k.html")
+    res.sendFile( __dirname + "/funcao_k.html");
 })
 
 //Outras fuções para testes!
 
 svc.get("/BuscaSequencial", function(req,res){
-    res.sendFile( __dirname + "/BuscaSequencial.html")
+    res.sendFile( __dirname + "/BuscaSequencial.html");
 })
 svc.get("/uvw", function(req,res){
-    res.send("<h1> Olá Tudo Bem?</h1>")
+    res.send("<h1> Olá Tudo Bem?</h1>");
 
 })
 svc.get("/soma", function(req,res){
-    res.sendFile( __dirname + "/soma.html")
+    res.sendFile( __dirname + "/soma.html");
 })
 
 
